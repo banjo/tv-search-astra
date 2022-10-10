a7061bb955580012b745a119f49bccdf604c17d2
Anton Ödman <anton.odman@layer10.se>
2022-10-10 10:50:52 +0200

    bug: quick and dirty ts fix for build

--- src/components/shared/Boop.tsx
+++ src/components/shared/Boop.tsx
@@ -3,11 +3,11 @@ import useBoop, { BoopConfig } from "../../hooks/useBoop";
 
 interface Props {
     children: JSX.Element[] | JSX.Element;
-    config?: BoopConfig;
+    config: BoopConfig;
 }
 
 const Boop = ({ children, config }: Props) => {
-    const [style, trigger] = useBoop(config);
+    const [style, trigger] = useBoop(config) as any[];
     return (
         <animated.span onMouseEnter={trigger} style={style}>
             {children}
